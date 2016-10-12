989d95d88919d83e1909a89fccb26380a03e6e63
silphashree <roshh1@gmail.com>
2016-10-12 14:28:21 -0400

    added details

---
+++ js/jquery.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $(".item01").slideUp(0).slideDown(600);
+    $(".item02").slideUp(0).delay(800).slideDown(600);
+    $(".item03").slideUp(0).delay(1400).slideDown(600); 
+    
+});
+
